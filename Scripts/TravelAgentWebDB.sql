3b12610b990c103ae8d398856e4172d81f00934e
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-09-18 18:33:51 -0400

    sec6cap33ok

--- Scripts/TravelAgentWebDB.sql
+++ Scripts/TravelAgentWebDB.sql
@@ -1,7 +1,7 @@
 USE TravelAgentWebDB
 GO
 
-INSERT INTO dbo.SubscriptionSecrets(secret, publisher)
-VALUES('3934bdb5-7edc-43d2-87dd-ee6f98f71720', 'PanAustralian')
+--INSERT INTO dbo.SubscriptionSecrets(secret, publisher)
+--VALUES('3934bdb5-7edc-43d2-87dd-ee6f98f71720', 'PanAustralian')
 
 SELECT * FROM dbo.SubscriptionSecrets
